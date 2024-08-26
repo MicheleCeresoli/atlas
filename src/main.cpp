6e2cfb3cb3b083831fd79d3d8786565415f76f16
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-08-26 15:11:49 +0200

    minro

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,98 @@
 
-
-#include "affine.h"
-#include "vec2.h"
-#include "vec3.h"
-#include "utils.h"
-#include "dcm.h"
 #include "camera.h"
-#include "renderer.h"
-#include "world.h"
-
-#include "pool.h"
+#include "dcm.h"
 #include "pixel.h"
 #include "raster.h"
-#include "crsutils.h"
+#include "renderer.h"
+#include "utils.h"
+#include "vec2.h"
+#include "vec3.h"
+#include "world.h"
 
-#include "gdal_priv.h"
-
+#include <algorithm>
 #include <cmath>
-#include <errno.h>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <vector>
-#include <chrono>
-#include <thread>
 
-#include <fstream>
 
 #define CAM_FOV (40) 
 
-void write_pixel(std::ostream& out, const PixelData& data) {
-    
-    auto g = 0.0;
-    auto b = 0.0;
-
-    auto r = data.t;
-    if (r == inf) {
-        r = 0.0;
-    } else {
-        r /= 10e3;
-        g = r; 
-        b = r; 
-    }
-
+void writePixel(std::ostream& out, double x) {
     // Translate the [0,1] component values to the byte range [0,255]
-    int rbyte = int(255.999*r);
-    int gbyte = int(255.999*g);
-    int bbyte = int(255.999*b);
+    int rbyte = int(255.999*x);
+    int gbyte = int(255.999*x);
+    int bbyte = int(255.999*x);
 
     // Write out the pixel color components. 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n'; 
+}
+
+void writeImage(const Camera& cam, const std::vector<double>& data) 
+{   
+    std::cout << "P3\n" << cam.width << ' ' << cam.height << "\n255\n"; 
+    for (int j = 0; j < data.size(); j++) {
+        writePixel(std::cout, data[j]); 
+    }
 
 }
 
-void toImage(Camera& cam, const std::vector<RenderedPixel> pixels)
+void makeImageDEM(const Camera& cam, const std::vector<RenderedPixel>& pixels)
 {
+    // First, we retrieve the minimum and maximum values of the data 
+    auto minmax = std::minmax_element(
+        pixels.begin(), pixels.end(),
+        [](const RenderedPixel& p1, const RenderedPixel& p2) {
+            return p1.d.s[0] < p2.d.s[0]; 
+        }
+    );
 
-    std::cout << "P3\n" << cam.width << ' ' << cam.height << "\n255\n"; 
+    std::clog << "The min height is: " << (*minmax.first).d.s[0] << std::endl; 
+    std::clog << "The max height is: " << (*minmax.second).d.s[0] << std::endl; 
 
-    for (int j = 0; j < pixels.size(); j++)
-    {
-        write_pixel(std::cout, pixels[j].d);
+    double min = minmax.first->d.s[0]; 
+    double max = minmax.second->d.s[0]; 
+
+    std::vector<double> imgData; 
+    imgData.reserve(pixels.size());
+
+    double t; 
+    for (auto p : pixels) {
+        t = (p.d.s[0] - min)/(max - min);  
+        imgData.push_back(t); 
     }
 
-    std::clog << "\rDone. \n";
+    writeImage(cam, imgData); 
+
+}
+
+void makeImageLIDAR(const Camera& cam, const std::vector<RenderedPixel>& pixels)
+{
+
+    // First, we retrieve the minimum and maximum values of the data 
+    auto minmax = std::minmax_element(
+        pixels.begin(), pixels.end(),
+        [](const RenderedPixel& p1, const RenderedPixel& p2) {
+            return p1.d.t < p2.d.t; 
+        }
+    );
+
+    std::clog << "The minimum altitude is: " << (*minmax.first).d.t << std::endl; 
+    std::clog << "The maximum altitude is: " << (*minmax.second).d.t << std::endl; 
+
+    double min = minmax.first->d.t; 
+    double max = minmax.second->d.t; 
+
+    std::vector<double> imgData; 
+    imgData.reserve(pixels.size());
+
+    double t; 
+    for (auto p : pixels) {
+        t = (p.d.t - min)/(max - min);  
+        imgData.push_back(t); 
+    }
+
+    writeImage(cam, imgData); 
 
 }
 
@@ -93,7 +123,7 @@ int main(int argc, const char* argv[])
 
     // double h = 1.1*1738e3/std::sin(cam_fov/2);
 
-    double h = 10e3 + 1737400;
+    double h = 40e3 + 1737400;
     double lon = deg2rad(-165);
     double lat = deg2rad(62);
 
@@ -104,8 +134,8 @@ int main(int argc, const char* argv[])
     // dcm    cam_dcm = dcm(0, 0, -1, 0, 1, 0, 1, 0, 0);
     
     dcm cam_dcm = pos2dcm(cam_pos); 
-    std::clog << cam_pos << std::endl;
-    std::clog << cam_dcm << std::endl;
+    std::clog << "Position: " << std::endl << cam_pos << "\n\n"; 
+    std::clog << "DCM: "  << std::endl << cam_dcm << std::endl; 
 
     // Initialise the camera object
     Camera cam(cam_res, cam_fov);
@@ -114,7 +144,7 @@ int main(int argc, const char* argv[])
 
     int nThreads = 7;   
 
-    std::string filename = "../CE2_GRAS_DEM_50m_C001_63N165W_A.tif";
+    std::string filename = "../resources/CE2_GRAS_DEM_50m_C001_63N165W_A.tif";
     RasterFile raster(filename, nThreads); 
     raster.loadBands(); 
 
@@ -124,14 +154,11 @@ int main(int argc, const char* argv[])
     Renderer renderer(nThreads, 640);
 
     // Render the image
-    auto t1 = std::chrono::high_resolution_clock::now();
     std::vector<RenderedPixel> pixels = renderer.render(cam, w); 
-    auto t2 = std::chrono::high_resolution_clock::now();
-
-    // FIXME: pixel color is divided by a fucking great distance
 
     // Write the pixels to a PPM image file
-    toImage(cam, pixels); 
+    // makeImageLIDAR(cam, pixels); 
+    makeImageDEM(cam, pixels); 
 
     return 0;
 }
