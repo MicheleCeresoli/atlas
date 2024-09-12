2ea3da96a3c3787c62250a1f8dafc0bb6e41ac2f
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-09-12 11:34:13 +0200

    Added options importation from YAML

--- python/arcadia/__init__.py
+++ python/arcadia/__init__.py
@@ -1,2 +1,88 @@
+# import _arcadia  # type: ignore
 
-from ._arcadia import * # type: ignore
+from ._arcadia import PinholeCamera, RealCamera         # type: ignore
+from ._arcadia import LunarRayTracer                    # type: ignore
+from ._arcadia import DEM, DOM                          # type: ignore
+from ._arcadia import LogLevel                          # type: ignore
+
+import glob 
+import yaml
+
+def RayTracerOptions(filename: str): 
+        
+    # Configuration data 
+    with open(filename, 'r') as file:
+        config = yaml.safe_load(file)
+    
+    # Retrieve number of threads 
+    if 'n-threads' in config.keys(): 
+        nthreads = config['n-threads']
+    else: 
+        nthreads = 1 
+        
+    # Retrieve log-level 
+    if 'log-level' in config.keys():
+        log_level = LogLevel(config['log-level'])
+    else: 
+        log_level = LogLevel.MINIMAL   
+
+    opts = _arcadia.RayTracerOptions(nthreads, log_level)
+    
+    # Retrieve rendering options
+    if 'rendering' in config.keys(): 
+        cfg_renderer = config['rendering'] 
+        
+        if 'batch-size' in cfg_renderer.keys(): 
+            opts.optsRenderer.batchSize = cfg_renderer['batch-size']
+            
+        if 'adaptive-tracing' in cfg_renderer.keys(): 
+            opts.optsRenderer.adaptiveTracing = cfg_renderer['adaptive-tracing']
+            
+        # Retrieve Antialiasing (SSAA) options
+        if 'ssaa' in cfg_renderer.keys():
+            cfg_ssaa = cfg_renderer['ssaa']
+            
+            for (key, val) in cfg_ssaa.items():
+                if key == 'active': 
+                    opts.optsRenderer.ssaa.active = val
+                    
+                elif key == 'subsamples': 
+                    opts.optsRenderer.ssaa.nSamples = val 
+                    
+                elif key == 'threshold': 
+                    opts.optsRenderer.ssaa.threshold = val 
+                    
+                elif key == 'res-multiplier': 
+                    opts.optsRenderer.ssaa.resMultiplier = val 
+                    
+                elif key == 'boundary-size': 
+                    opts.optsRenderer.ssaa.boundarySize = val
+   
+    # Retrieve world settings 
+    if 'world' in config.keys(): 
+        cfg_world = config['world']
+
+        if 'dem-files' in cfg_world.keys(): 
+            
+            dem_files = []
+            for file in cfg_world['dem-files']: 
+                dem_files += glob.glob(file)
+                
+            # avoid duplicate files
+            opts.optsWorld.demFiles = list(set(dem_files))
+                
+        if 'dom-files' in cfg_world.keys(): 
+            
+            dom_files = []
+            for file in cfg_world['dom-files']: 
+                dom_files += glob.glob(file)
+                
+            # avoid duplicate files
+            opts.optsWorld.domFiles = list(set(dom_files))
+        
+        if 'raster-usage-threshold' in cfg_world.keys(): 
+            opts.optsWorld.rasterUsageThreshold = cfg_world['raster-usage-threshold']
+        
+    
+    return opts 
+    
