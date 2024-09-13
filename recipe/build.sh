d5147aa54888bb65a19679056ee137a3a08c022d
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-09-13 23:29:49 +0200

    Added conda recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash 
+# Set the CMake generator to Ninja
+export CMAKE_GENERATOR=Ninja
 # We ensure it doesn't install any dependencies sine Conda handles them already
 $PYTHON -m pip install . --no-deps
