540b72aef028b5c37fc1fc7ed1e85282253947a2
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-09-15 22:18:04 +0200

    Initialising Documentation

--- include/atlas/affine.h
+++ include/atlas/affine.h
@@ -5,31 +5,83 @@
 #include <iostream> 
 #include <string>
 
+/**
+ * @class Affine
+ * @brief Class representing an affine transformation.
+ * 
+ */
 class Affine {
 
     public: 
 
+        /**
+         * @brief Construct a new Affine object representing an identity transform.
+         */
         Affine(); 
+
+        /**
+         * @brief Construct a new Affine object by specifing all the parameters.
+         * 
+         * @param e0 
+         * @param e1 
+         * @param e2 
+         * @param e3 
+         * @param e4 
+         * @param e5 
+         */
         Affine(double e0, double e1, double e2, double e3, double e4, double e5);
+
+        /**
+         * @brief Construct a new Affine object from a vector pointer.
+         * @param p 
+         */
         Affine(double* p);
-
-        double operator[](int i) const; 
-        double& operator[](int i); 
-
-        Affine& operator*=(const Affine& a); 
 
         // Return x and y offsets
         double xoff() const; 
         double yoff() const; 
 
-        // Compute the determinant of the transformation
+        /**
+         * @brief Compute the determinant of the transformation
+         * 
+         * @return double 
+         */
         double det() const; 
-
+        
+        /**
+         * @brief Return a string representing the affine transformation.
+         * @return std::string 
+         */
         std::string toString() const; 
 
+        /**
+         * @brief 
+         * 
+         * @return Affine 
+         */
         static Affine identity(); 
+
+        /**
+         * @brief 
+         * 
+         * @param s 
+         * @return Affine 
+         */
         static Affine scale(double s);
+
+        /**
+         * @brief 
+         * 
+         * @param s1 
+         * @param s2 
+         * @return Affine 
+         */
         static Affine scale(double s1, double s2); 
+
+        double operator[](int i) const; 
+        double& operator[](int i); 
+
+        Affine& operator*=(const Affine& a); 
 
     private: 
         double e[6];
@@ -41,6 +93,12 @@ std::ostream& operator<<(std::ostream& out, const Affine& a);
 Affine operator*(const Affine& a, const Affine &b); 
 vec2 operator*(const Affine& a, const vec2& v);
 
+/**
+ * @brief Compute the inverse of an affine transformation.
+ * 
+ * @param a Input affine transformation.
+ * @return Affine 
+ */
 Affine inverse(const Affine& a); 
 
 #endif 
--- include/atlas/vec2.h
+++ include/atlas/vec2.h
@@ -5,17 +5,60 @@
 #include <iostream> 
 #include <string>
 
+/**
+ * @class vec2 
+ * @brief Class representing a 2-dimensional vector.
+ */
 class vec2 {
 
     public: 
 
+        /**
+         * @brief Array storing the vector components.
+         */
         double e[2]; 
 
+        /**
+         * @brief Construct a new vec2 object with null components.
+         */
         vec2();
+
+        /**
+         * @brief Construct a new vec2 object.
+         * @param e0 Value of the x-component.
+         * @param e1 Value of the y-component.
+         */
         vec2(double e0, double e1);
 
+        /**
+         * @brief Return the x component of the vector.
+         * @return double 
+         */
         double x() const;
+       
+        /**
+         * @brief Return the y component of the vector.
+         * @return double 
+         */
         double y() const;
+
+        /**
+         * @brief Compute the Euclidean norm of the vector.
+         * @return double 
+         */
+        inline double norm() const { return std::sqrt(norm2()); };
+
+        /**
+         * @brief Compute the squared Euclidean norm of the vector.
+         * @return double 
+         */
+        inline double norm2() const { return e[0]*e[0] + e[1]*e[1]; };
+
+        /**
+         * @brief Return a string representing the vector content.
+         * @return std::string 
+         */
+        std::string toString() const; 
 
         vec2 operator-() const;
         
@@ -25,11 +68,6 @@ class vec2 {
         vec2& operator+=(const vec2& v);
         vec2& operator*=(double t);
         vec2& operator/=(double t);
-
-        inline double norm() const { return std::sqrt(norm2()); };
-        inline double norm2() const { return e[0]*e[0] + e[1]*e[1]; };
-
-        std::string toString() const; 
 
 };
 
@@ -47,8 +85,21 @@ vec2 operator*(const vec2& v, double t);
 
 vec2 operator/(const vec2& v, double t);
 
+/**
+ * @brief Normalise a vector.
+ * 
+ * @param v Input vector to be normalised.
+ * @return vec2 
+ */
 vec2 unit_vector(const vec2& v);
 
+/**
+ * @brief Compute the dot product between two vectors.
+ * 
+ * @param u First vector.
+ * @param v Second vector.
+ * @return double 
+ */
 double dot(const vec2& u, const vec2& v);
 
 // point2 is an alias for vec2 
