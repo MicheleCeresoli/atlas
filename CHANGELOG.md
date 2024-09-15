998efc5dc589196722e5b8719b8fd9ceecbe8273
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-09-15 15:48:17 +0200

    Updated to v0.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # All Releases 
 
+## v0.4.1
+- Replaced all `ui16_t` instances to `ui32_t` to avoid overflow errors.
+- Added error information when tracing without any loaded DEM rasters.
+
 ## v0.4.0
 - Added conda recipe.
 - Added `dem-path` and `dom-path` settings to YAML configuration file.
--- apps/main.cpp
+++ apps/main.cpp
@@ -1,13 +1,18 @@
+// #include "atlas.h"
+// #include "camera.h"
+// #include "utils.h"
+
 #include <iostream>
+
+// #include <string>
 
 // dcm pos2dcm(point3 pos)
 // {
 //     vec3 uz = -unit_vector(pos);
     
 //     vec3 k(0.0, 0.0, 1.0); 
-//     vec3 uy = unit_vector(cross(uz, k)); 
-    
-//     vec3 ux = unit_vector(cross(uy, uz)); 
+//     vec3 ux = unit_vector(cross(k, uz)); 
+//     vec3 uy = unit_vector(cross(uz,ux)); 
 
 //     return dcm(
 //         ux[0], uy[0], uz[0], 
@@ -17,6 +22,42 @@
 // }
 
 int main(int argc, const char* argv[]) {
+
+    // RayTracerOptions opts(6, LogLevel::DETAILED);
+
+    // std::vector<std::string> dem_files; 
+    // std::vector<std::string> dom_files; 
+
+    // dem_files.push_back(
+    //     "/home/michele/phd/snoopy/resources/dem/CE2_GRAS_DEM_50m_B001_77N158W_A.tif"
+    // );
+    
+    // dom_files.push_back(
+    //     "/home/michele/phd/snoopy/resources/dom/CE2_GRAS_DOM_50m_B001_77N158W_A.tif"
+    // );
+
+    // opts.optsWorld.demFiles = dem_files; 
+    // opts.optsWorld.domFiles = dom_files; 
+
+    // LunarRayTracer tracer(opts); 
+
+    // double lon = deg2rad(-161); 
+    // double lat = deg2rad(77); 
+
+    // double r = 50e3 + 1737400;
+
+    // vec3 camPos = sph2car(vec3(r, lon, lat));
+    // dcm  camDCM = pos2dcm(camPos);
+
+    // PinholeCamera cam(640, deg2rad(40));
+    // cam.setPos(camPos);
+    // cam.setDCM(camDCM);
+
+    // tracer.updateCamera(&cam); 
+    // tracer.run(); 
+
+    // tracer.generateImageOptical("00000.png");
+    // tracer.generateGCPs("00000.csv", 16);
 
     return 0;
 
