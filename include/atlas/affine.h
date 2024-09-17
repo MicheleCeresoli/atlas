1ce88d953fd7bf4d2c3ad0cb2038d071896a6fe0
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-09-17 22:44:58 +0200

    Added docs for `Affine`

--- include/atlas/affine.h
+++ include/atlas/affine.h
@@ -22,18 +22,19 @@ class Affine {
         /**
          * @brief Construct a new Affine object by specifing all the parameters.
          * 
-         * @param e0 
-         * @param e1 
-         * @param e2 
-         * @param e3 
-         * @param e4 
-         * @param e5 
+         * @param e0 pixel size in the x-direction in map units/pixel.
+         * @param e1 rotation about the x-axis. 
+         * @param e2 x-coordiante of the center of the upper left pixel. 
+         * @param e3 rotation about the y-axis.
+         * @param e4 pixel size in the y-direction in map units/pixel.
+         * @param e5 y-coordinate of the center of the upper left pixel.
          */
         Affine(double e0, double e1, double e2, double e3, double e4, double e5);
 
         /**
          * @brief Construct a new Affine object from a vector pointer.
-         * @param p 
+         * @param p pointer to a double vector of 6 elements.
+         * @warning No check is performed on the actual dimension of the array.
          */
         Affine(double* p);
 
@@ -42,7 +43,7 @@ class Affine {
         double yoff() const; 
 
         /**
-         * @brief Compute the determinant of the transformation
+         * @brief Compute the determinant of the transformation.
          * 
          * @return double 
          */
@@ -55,25 +56,25 @@ class Affine {
         std::string toString() const; 
 
         /**
-         * @brief 
+         * @brief Return an identity affine transformation.
          * 
          * @return Affine 
          */
         static Affine identity(); 
 
         /**
-         * @brief 
+         * @brief Return an affine transformation that equally scales both axes.
          * 
-         * @param s 
+         * @param s scale factor.
          * @return Affine 
          */
         static Affine scale(double s);
 
         /**
-         * @brief 
+         * @brief Return an affine transformation that scales the x and y axes.
          * 
-         * @param s1 
-         * @param s2 
+         * @param s1 scale factor on the x-axis.
+         * @param s2 scale factor on the y-axis.
          * @return Affine 
          */
         static Affine scale(double s1, double s2); 
@@ -90,7 +91,22 @@ class Affine {
 
 std::ostream& operator<<(std::ostream& out, const Affine& a);
 
+/**
+ * @brief Compute the product between two affine transformations.
+ * 
+ * @param a First affine transformation. 
+ * @param b Second affine transformation. 
+ * @return Affine 
+ */
 Affine operator*(const Affine& a, const Affine &b); 
+
+/**
+ * @brief Apply the affine transformation to a 2-dimensional point.
+ * 
+ * @param a Affine transformation. 
+ * @param v 2-dimensional vector.
+ * @return vec2 
+ */
 vec2 operator*(const Affine& a, const vec2& v);
 
 /**
