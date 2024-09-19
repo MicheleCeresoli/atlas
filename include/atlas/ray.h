15a9f160bc7484585039302ebc0d79ab0b0c7e01
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-09-19 22:35:58 +0200

    Added docs for `Ray`

--- include/atlas/ray.h
+++ include/atlas/ray.h
@@ -9,15 +9,52 @@
  */
 class Ray {
     public: 
-    
+
+        /**
+         * @brief Construct a new Ray object.
+         * 
+         * @param origin Ray origin point.
+         * @param direction Ray direction. The constructor will automatically take care of 
+         * normalising the input vector.
+         */
         Ray(const point3& origin, const vec3& direction);
 
+        /**
+         * @brief Return the ray origin point.
+         * @return const point3& origin.
+         */
         const point3& origin() const;
+
+        /**
+         * @brief Return the ray direction.
+         * @return const vec3& ray direction.
+         */
         const vec3& direction() const;
 
+        /**
+         * @brief Compute the position along the ray at a given distance from the origin.
+         * 
+         * @param t Distance from the origin, in world units, at which to evalute the ray 
+         * position. If a negative value is provided, the position will be computed in a 
+         * direction opposite to the one provided.
+         * @return point3 3-dimensional position.
+         */
         point3 at(double t) const;
-
+        
+        /**
+         * @brief Return the minimum distance, in world units, from the center of the world.
+         * @return double Minimum ray distance. 
+         */
         double minDistance() const;
+
+        /**
+         * @brief Return the parametric values that correspond to a given distance of the 
+         * ray from the world origin.
+         * 
+         * @param r distance from world origin.
+         * @param tMin t-value at the first crossing.
+         * @param tMax t-value at the second crossing. 
+         */
         void getParameters(double r, double& tMin, double& tMax) const;
 
     private: 
