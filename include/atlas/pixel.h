8bbd8a6aca56cc02a1f832045c60b50974700338
MicheleCeresoli <michele.ceresoli@polimi.it>
2024-09-19 22:54:13 +0200

    Added docs for `RenderedPixel`

--- include/atlas/pixel.h
+++ include/atlas/pixel.h
@@ -71,28 +71,88 @@ struct PixelData {
     point3 s;       // Spherical coordinates of the intersection point
 };
 
+/**
+ * @class RenderedPixel
+ * @brief Class storing all the rendered information for a given pixel.
+ */
 class RenderedPixel {
 
     public: 
 
-        ui32_t id;                        // Pixel id
+        /**
+         * @brief Pixel ID
+         */
+        ui32_t id;        
+        
+        /**
+         * @brief Number of samples taken for this pixel.
+         */
         size_t nSamples;
 
+        /**
+         * @brief Rendered pixel data for each sample.
+         */
         std::vector<PixelData> data;
 
+        /**
+         * @brief Construct a new Rendered Pixel object.
+         * 
+         * @param id Rendered pixel ID.
+         * @param nSamples Number of rendered samples for this pixel.
+         */
         RenderedPixel(ui32_t id, size_t nSamples);
+
+        /**
+         * @brief Automatically construct a new RenderedPixel object from a TaskedPixel. 
+         * @param pix Tasked pixel.
+         * 
+         * @note The new object automatically inherits the ID and the number of pixel 
+         * samples from the input TaskedPixel.
+         */
         inline RenderedPixel(TaskedPixel pix) : RenderedPixel(pix.id, pix.nSamples) {};
 
+        /**
+         * @brief Update the number of samples taken for this pixel.
+         * @details This function only updates the total number of samples and reserves 
+         * enough space within the internal vectors. 
+         * @param newSamples Total number of rendered samples.
+         */
         void updateSamples(size_t newSamples); 
+
+        /**
+         * @brief Add new rendered information. 
+         * @param d Rendered pixel data.
+         */
         void addPixelData(const PixelData& d); 
 
         // Return the distance of the first pixel sample (i.e., the ray from the center)
+
+        /**
+         * @brief Retun the distance of the first pixel sample (i.e., the ray from the 
+         * center of the aperture to the center of the pixel.)
+         * 
+         * @return double Ray distance.
+         */
         inline double pixDistance() const { return data[0].t; } 
 
+        /**
+         * @brief Return the minimum ray distance across all pixel samples.
+         * @return double Minimum ray distance.
+         */
         inline double pixMinDistance() const { return tMin; }; 
+
+        /**
+         * @brief Return the maximum ray distance across all pixel samples.
+         * @return double Maximum ray distance.
+         */
         inline double pixMaxDistance() const { return tMax; }; 
 
-        // Return the averaged distance across all pixel samples
+        /**
+         * @brief Return the averaged distance across all pixel samples.
+         * @return double Average distance.
+         * 
+         * @note Samples that have an infinite ray distance are excluded from the mean. 
+         */
         double pixMeanDistance() const; 
 
     private: 
