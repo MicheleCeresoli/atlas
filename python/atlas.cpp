443d87905f69433a5938a1d5ece834a57d14ecd5
MicheleCeresoli <michele.ceresoli@polimi.it>
2025-06-12 20:30:52 +0200

    Updated image conversion

--- python/atlas.cpp
+++ python/atlas.cpp
@@ -6,6 +6,33 @@
 
 namespace py = pybind11;
 
+py::array cvMatToNumpy(const cv::Mat& mat) {
+
+    // Determine the numpy array type
+    py::dtype dtype; 
+    switch (mat.depth()) {
+        case CV_8UC1: dtype = py::dtype::of<uint8_t>(); break; 
+        case CV_16UC1: dtype = py::dtype::of<uint16_t>(); break;
+        default: 
+            throw std::runtime_error("Unsupported image type.");
+    }
+
+    // Use a shared pointer to ensure memory remains valid 
+    auto mat_ptr = std::make_shared<cv::Mat>(mat);
+
+    // py::capsule nesure cv::Mat is alive until Python is done with the data
+    return py::array(
+        dtype, 
+        {mat.rows, mat.cols}, 
+        {mat.step[0], mat.step[1]}, 
+        mat_ptr->data,
+        py::capsule(mat_ptr.get(), [](void* p) {
+            delete static_cast<cv::Mat*>(p);
+        })
+    );
+
+}
+
 void init_atlas(py::module_ &m) {
 
 
@@ -26,11 +53,29 @@ void init_atlas(py::module_ &m) {
         .def("updateCameraPosition", &RayTracer::updateCameraPosition)
         .def("updateCameraOrientation", &RayTracer::updateCameraOrientation)
 
-        .def("createImageOptical", &RayTracer::createImageOptical, py::arg("type") = CV_8UC1)
-        .def("createDepthMap", &RayTracer::createDepthMap, py::arg("type") = CV_8UC1)
-        .def("createImageDEM", &RayTracer::createImageDEM, 
-            py::arg("type") = CV_8UC1, py::arg("normalize") = true
-        )
+        .def("createImageOptical", [](RayTracer& self, int type) -> py::array {
+            
+            // Generate the image and convert it to a numpy array
+            cv::Mat img = self.createImageOptical(type);
+            return cvMatToNumpy(img);
+
+        }, py::arg("type") = CV_8UC1)
+
+        .def("createDepthMap", [](RayTracer& self, int type) -> py::array {
+
+            // Generate the image and convert it to a numpy array 
+            cv::Mat img = self.createDepthMap(type); 
+            return cvMatToNumpy(img);
+
+        }, py::arg("type") = CV_8UC1)
+            
+        .def("createImageDEM", [](RayTracer& self, int type, bool normalize) -> py::array {
+
+            // Generate the image and convert it to a numpy array 
+            cv::Mat img = self.createImageDEM(type, normalize); 
+            return cvMatToNumpy(img);
+
+        }, py::arg("type") = CV_8UC1, py::arg("normalize") = true)
         
         .def("saveImageOptical", &RayTracer::saveImageOptical, 
             py::arg("filename"), py::arg("type") = CV_8UC1
