bc3bd469eae5e04f701ad7f53c9c0f8e18c34013
MicheleCeresoli <michele.ceresoli@polimi.it>
2025-09-09 14:30:36 +0200

    WIP

--- include/atlas/grid.h
+++ include/atlas/grid.h
@@ -2,6 +2,7 @@
 #ifndef PIXELGRID_H 
 #define PIXELGRID_H
 
+#include "camera.h"
 #include "types.h"
 
 // TODO: add possibility to store the resolution 
@@ -11,7 +12,7 @@ class ScreenGrid {
 
     public: 
 
-    ScreenGrid(ui32_t width, ui32_t height); 
+    ScreenGrid(ui32_t width, ui32_t height, const Camera* cam); 
 
     virtual ~ScreenGrid() = default; 
 
@@ -20,11 +21,19 @@ class ScreenGrid {
 
     inline ui32_t nPixels() const { return _width*_height; }
 
+    ui32_t getPixelId(const ui32_t& u, const ui32_t& v); 
     
     private: 
 
         ui32_t _width = 0; 
         ui32_t _height = 0;
+
+        // Coordinates of the top-left pixel 
+        ui32_t u0, v0; 
+
+        // Camera width and height
+        ui32_t cam_width; 
+        ui32_t cam_height;
 
 };
 
