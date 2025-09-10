33568ae280d8bbbbf62a9d215c03fff07dd1b4c6
MicheleCeresoli <michele.ceresoli@polimi.it>
2025-09-10 15:21:55 +0200

    Added `ScreenGrid`

--- include/atlas/grid.h
+++ include/atlas/grid.h
@@ -3,39 +3,75 @@
 #define PIXELGRID_H
 
 #include "camera.h"
+#include "pixel.h"
 #include "types.h"
 
-// TODO: add possibility to store the resolution 
-// TODO: add capability to convert pixel ID to camera / cell 
 
 class ScreenGrid {
 
     public: 
 
-    ScreenGrid(ui32_t width, ui32_t height, const Camera* cam); 
 
-    virtual ~ScreenGrid() = default; 
+        ScreenGrid(Pixel p0, ui32_t width, ui32_t height, const Camera* cam); 
 
-    inline ui32_t width() const { return _width; }
-    inline ui32_t height() const { return _height; }
+        virtual ~ScreenGrid() = default; 
 
-    inline ui32_t nPixels() const { return _width*_height; }
+        inline ui32_t width() const { return _width; }
+        inline ui32_t height() const { return _height; }
 
-    ui32_t getPixelId(const ui32_t& u, const ui32_t& v); 
-    
+        inline ui32_t nPixels() const { return _width*_height; }
+
+        inline Pixel topLeft() const { return p0; }
+
+        ui32_t getGPixelId(const ui32_t& ug, const ui32_t& vg) const; 
+        ui32_t getGPixelId(const ui32_t& gid) const;
+
+        inline ui32_t getGPixelId(const Pixel& pg) const { 
+            return getGPixelId(pg[0], pg[1]); 
+        }
+
+        inline ui32_t getGPixelGridId(const ui32_t& ug, const ui32_t& vg) const {
+            return ug + _width*vg;
+        }
+
+        inline ui32_t getGPixelGridId(const Pixel& pg) const { 
+            return getGPixelGridId(pg[0], pg[1]); 
+        }
+
+        void getGPixelCoordinates(const ui32_t& gid, ui32_t& u, ui32_t& v) const; 
+        void getGPixelGridCoordinates(const ui32_t& gid, ui32_t &ug, ui32_t& vg) const;
+
+        inline void setRayResolution(double res) { dt = res; }
+        inline double getRayResolution() const { return dt; }
+
+        inline void addRayDistance(double distance) { rayDistances.push_back(distance); }
+        inline const std::vector<double>* getRayDistances() const {
+            return &rayDistances;
+        }
+
+        bool isRowAdaptiveRendering(); 
+
+        
     private: 
 
+        // Screen grid width and height
         ui32_t _width = 0; 
         ui32_t _height = 0;
 
         // Coordinates of the top-left pixel 
-        ui32_t u0, v0; 
+        Pixel p0;
+        
+        // ID of the top-left pixel
+        ui32_t id0;
 
         // Camera width and height
         ui32_t cam_width; 
         ui32_t cam_height;
 
-};
+        // Ray resolutions
+        double dt; 
 
+        std::vector<double> rayDistances;
+};
 
 #endif
---
+++ python/grid.cpp
@@ -0,0 +1,53 @@
+
+#include <pybind11/pybind11.h> 
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h> 
+
+#include "grid.h"
+
+namespace py = pybind11;
+
+void init_grid(py::module_ &m) {
+
+    py::class_<ScreenGrid>(m, "ScreenGrid") 
+
+        .def(
+            py::init<Pixel, ui32_t, ui32_t, const Camera* cam>(), 
+            py::arg("p0"), py::arg("width"), py::arg("height"), py::arg("cam")
+        )
+
+        .def("width", &ScreenGrid::width)
+        .def("height", &ScreenGrid::height)
+        .def("nPixels", &ScreenGrid::nPixels)
+        .def("topLeft", &ScreenGrid::topLeft)
+
+        .def("getRayResolution", &ScreenGrid::getRayResolution)
+        .def("setRayResolution", &ScreenGrid::setRayResolution)
+
+        .def("isRowAdaptiveRendering", &ScreenGrid::isRowAdaptiveRendering)
+
+        .def("getGPixelCoordinates", &ScreenGrid::getGPixelCoordinates)
+        .def("getGPixelGridCoordinates", &ScreenGrid::getGPixelGridCoordinates)
+
+        .def("getGPixelId", py::overload_cast<const ui32_t&, const ui32_t&>(
+                &ScreenGrid::getGPixelId, py::const_
+        ))
+
+        .def("getGPixelId", py::overload_cast<const ui32_t&>(
+            &ScreenGrid::getGPixelId, py::const_
+        ))
+
+        .def("getGPixelId", py::overload_cast<const Pixel&>(
+            &ScreenGrid::getGPixelId, py::const_
+        ))
+        
+        .def("getGPixelGridId", py::overload_cast<const ui32_t&, const ui32_t&>(
+            &ScreenGrid::getGPixelGridId, py::const_
+        ))
+
+        .def("getGPixelGridId", py::overload_cast<const Pixel&>(
+            &ScreenGrid::getGPixelGridId, py::const_
+        ))
+        ;
+
+}
